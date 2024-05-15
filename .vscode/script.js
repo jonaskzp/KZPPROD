991c64009e4e73578c1c0bcfe5046c74dd451e35
jonaskzp <jonas.calil@hotmail.com>
2024-05-14 21:06:58 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,31 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //APOS PEGAR A TAG DA IMAGEM, SUBSTITUA
+  const img = document.querySelector("#PROFILE img")
+
+  //SUBSTITUA A IMAGEM
+  if (html.classList.contains("light")) {
+    //SE TIVER LIGTH MODE
+    img.setAttribute("src", "/assets/avatar.png ")
+  } else {
+    img.setAttribute("src", "/assets/avatar-light.png")
+  }
+}
+
+/*
+  
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+
+ ESSE CÓDIGO É O QUE FAZ O BOTAO DE JAVA FUNCIONAR. POREM POSSO FAZER ELE DE FORMA SIMPLIFICADA COMO ABAIXO*/
+
+/*PASSO  A PASSO PARA TROCAR A IMAGEM 
+  1PEGAR A TAG IMAGE
+  2SUBSTITUIR A IMAGEM
+  (CONDIÇAO- SE TIVER LIGTH MODE IMAGEM LIGTH, SEM LIGHT MODE MANTER IMAGEM NORMAL)
+  */
